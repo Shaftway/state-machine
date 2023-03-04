c44457b2fc0fa50b69d73f5aad4aac1c6f9b3186
Paul Hounshell <paul.hounshell@gmail.com>
2023-03-04 00:30:51 -0800

    Refactor StateMachine and the builder to make subclassing easier

--- java/com/hounshell/st8/CallbackWrapper.java
+++ java/com/hounshell/st8/CallbackWrapper.java
@@ -1,17 +1,11 @@
 package com.hounshell.st8;
 
 /* protected */ class CallbackWrapper<T, From extends T, To extends T> {
-    private final Transition transition;
-    private final Callback<From, To> callback;
+    /* protected */ final Transition transition;
+    /* protected */ final Callback<From, To> callback;
 
     /* protected */ CallbackWrapper(Transition transition, Callback<From, To> callback) {
         this.transition = transition;
         this.callback = callback;
-    }
-
-    /* protected */ void handleCall(T from, T to) {
-        if (transition.isValid(from, to)) {
-            callback.onStateChanged((From) from, (To) to);
-        }
     }
 }
--- java/com/hounshell/st8/StateMachine.java
+++ java/com/hounshell/st8/StateMachine.java
@@ -1,12 +1,8 @@
 package com.hounshell.st8;
 
-import static java.lang.System.lineSeparator;
-
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Enumeration;
-import java.util.HashSet;
 import java.util.LinkedHashMap;
+import java.util.function.Consumer;
 
 public class StateMachine<T> {
     private final ArrayList<Transition> validTransitions = new ArrayList<>();
@@ -15,13 +11,23 @@ public class StateMachine<T> {
     private T nextState;
     private boolean stateTransitioning = false;
 
-    private StateMachine(ArrayList<Transition> validTransitions, T currentState) {
-        this.validTransitions.addAll(validTransitions);
+    protected StateMachine(BaseBuilder<T, ?, ?> builder, T currentState) {
+        this.validTransitions.addAll(builder.validTransitions);
         this.currentState = currentState;
     }
 
     public T getCurrentState() {
         return currentState;
+    }
+
+    public boolean isState(Class<? extends T>... stateClasses) {
+        if (stateClasses != null) {
+            for (Class<?> clazz : stateClasses) {
+                if (clazz.isAssignableFrom(currentState.getClass())) return true;
+            }
+        }
+
+        return false;
     }
 
     public synchronized boolean isNextStateQueued() {
@@ -32,17 +38,17 @@ public class StateMachine<T> {
         transition(null, to);
     }
 
-    public synchronized void transition(T from, T to) {
+    public synchronized boolean transition(T from, T to) {
         assert (to != null);
 
         if (stateTransitioning && nextState != null) {
             throw new TransitionContentionException(currentState, nextState, to);
         }
 
-        if (from != null && from != currentState) return;
+        if (from != null && from != currentState) return false;
 
         nextState = to;
-        if (stateTransitioning) return;
+        if (stateTransitioning) return true;
 
         stateTransitioning = true;
 
@@ -56,7 +62,9 @@ public class StateMachine<T> {
 
                         // Iterate over a cloned list of callbacks in case they're modified.
                         for (CallbackWrapper<T, ?, ?> callback : new ArrayList<>(callbacks.values())) {
-                            callback.handleCall(oldState, currentState);
+                            if (callback.transition.isValid(oldState, currentState)) {
+                                castAndCallCallback(oldState, currentState, callback.callback);
+                            }
                         }
 
                         break found;
@@ -68,6 +76,15 @@ public class StateMachine<T> {
         }
 
         stateTransitioning = false;
+        return true;
+    }
+
+    private <From extends T, To extends T> void castAndCallCallback(T from, T to, Callback<From, To> callback) {
+        callCallback((From) from, (To) to, callback);
+    }
+
+    protected <From extends T, To extends T> void callCallback(From from, To to, Callback<From, To> callback) {
+        callback.onStateChanged(from, to);
     }
 
     public <From extends T, To extends T> CallbackToken addCallback(
@@ -77,9 +94,29 @@ public class StateMachine<T> {
         return token;
     }
 
+    public <From extends T, To extends T> CallbackToken addCallback(
+            Class<From> from, Class<To> to, Consumer<To> callback) {
+        return addCallback(from, to, (f, t) -> callback.accept(t));
+    }
+
+    public <From extends T, To extends T> CallbackToken addCallback(
+            Class<From> from, Class<To> to, Runnable callback) {
+        return addCallback(from, to, (f, t) -> callback.run());
+    }
+
     public <To extends T> CallbackToken addCallbackFromAnythingTo(
             Class<To> to, Callback<T, To> callback) {
         return addCallback(null, to, callback);
+    }
+
+    public <To extends T> CallbackToken addCallbackFromAnythingTo(
+            Class<To> to, Consumer<To> callback) {
+        return addCallbackFromAnythingTo(to, (f, t) -> callback.accept(t));
+    }
+
+    public <To extends T> CallbackToken addCallbackFromAnythingTo(
+            Class<To> to, Runnable callback) {
+        return addCallbackFromAnythingTo(to, (f, t) -> callback.run());
     }
 
     public <From extends T, To extends T> CallbackToken addCallbackToAnythingFrom(
@@ -95,30 +132,41 @@ public class StateMachine<T> {
         callbacks.remove(token);
     }
 
-    public static class Builder<T> {
+    public static <T> Builder<T> newBuilder() {
+        return new Builder<>();
+    }
+
+    public static final class Builder<T> extends BaseBuilder<T, StateMachine<T>, Builder<T>> {
+        @Override
+        public StateMachine<T> buildWithInitialState(T state) {
+            return new StateMachine<>(this, state);
+        }
+    }
+
+    public abstract static class BaseBuilder<T, SM extends StateMachine<T>, B extends BaseBuilder<T, SM, B>> {
         private final ArrayList<Transition> validTransitions = new ArrayList<>();
 
-        public <To extends T> Builder<T> addValidTransitionFromAnythingTo(Class<To> to) {
+        protected BaseBuilder() {}
+
+        public <To extends T> B addValidTransitionFromAnythingTo(Class<To> to) {
             return addValidTransition(null, to);
         }
 
-        public <From extends T> Builder<T> addValidTransitionToAnythingFrom(Class<From> from) {
+        public <From extends T> B addValidTransitionToAnythingFrom(Class<From> from) {
             return addValidTransition(from, null);
         }
 
-        public <From extends T, To extends T> Builder<T> addValidTransition(Class<From> from, Class<To> to) {
+        public <From extends T, To extends T> B addValidTransition(Class<From> from, Class<To> to) {
             validTransitions.add(new Transition(from, to));
-            return this;
+            return (B) this;
         }
 
-        public Builder<T> makeAllTransitionsValid() {
+        public B makeAllTransitionsValid() {
             validTransitions.clear();
             validTransitions.add(new Transition(null, null));
-            return this;
+            return (B) this;
         }
 
-        public StateMachine<T> buildWithInitialState(T state) {
-            return new StateMachine<T>(validTransitions, state);
-        }
+        public abstract SM buildWithInitialState(T state);
     }
 }
